90fb4a1bea0a94ba20cbf76a0adf8ee918b140ff
Tomáš Mikula <mail@tomasmikula.cz>
2021-10-17 14:04:08 +0200

    Update

--- tests/test_submatrix.py
+++ tests/test_submatrix.py
@@ -6,10 +6,10 @@ from algebra.matrix import submatrix
 @pytest.mark.parametrize(
     "matrix,drop_rows,drop_columns,expected",
     [
-        ([[1,2,3], [1,2,3]], [], [0], [[2,3], [2,3]]),
-        ([[1,2,3], [1,2,3]], [0], [0], [[2,3]]),
-        ([[1,2,3], [1,2,3]], [0], [0,1], [[3]]),
-        ([[1,2,3], [1,2,3]], [0], [0,1,2], []),
+        ([[1, 2, 3], [1, 2, 3]], [], [0], [[2, 3], [2, 3]]),
+        ([[1, 2, 3], [1, 2, 3]], [0], [0], [[2, 3]]),
+        ([[1, 2, 3], [1, 2, 3]], [0], [0, 1], [[3]]),
+        ([[1, 2, 3], [1, 2, 3]], [0], [0, 1, 2], []),
     ],
 )
 def test_submatrix(matrix, drop_rows, drop_columns, expected):
@@ -17,8 +17,8 @@ def test_submatrix(matrix, drop_rows, drop_columns, expected):
 
 
 def test_submatrix_identity():
-    matrix = [[1,2,3], [1,2,3]]
+    matrix = [[1, 2, 3], [1, 2, 3]]
 
     result = submatrix(matrix, [], [])
-    
+
     assert matrix is not result
